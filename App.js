ba4d1590e1a0666cbb0a6ceb13ae0143974e9d97
myk4040okothogodo <mikeogodo5@gmail.com>
2022-02-11 09:29:42 +0300

    onboarding

--- App.js
+++ App.js
@@ -1,112 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    Image,
+    TouchableOpacity
 } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme} from "@react-navigation/native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+//screens
+import {Onboarding, DestinationDetail} from "./screens";
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const theme ={
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    }
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Stack = createStackNavigator();
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+const App = () => {
+    return (
+        <NavigationContainer theme={theme}>
+          <Stack.Navigator
+              initialRouteName("")
+          >
+            {/* Screens */}
+            <Stack.Screen 
+               name = "Onboarding"
+               component = {Onboarding}
+               options = {{
+                  title: null,
+                  headerStyle: {
+                      backgroundColor: COLORS.white
+                  },
+                   headerLeft: null,
+                   headerRight: () => {
+                       <TouchableOpacity
+                           style={{ marginRight: SIZES.padding}}
+                           onPress = {() => console.log("Pressed") }
+                       >
+                           <Image 
+                               source = {icons.barMenu}
+                               resizeMode = "contain"
+                               style = {{
+                                 width: 25,
+                                 height: 25,
+                               }}
+                           />
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+                       </TouchableOpacity>
+                   }
+               }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+
+
+    )
+
+}
 
 export default App;
---
+++ screens/DestinationDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+const DestinationDetail = () => {
+    return (
+      <View>
+        <Text>DestinationDetail</Text>
+      </View>
+    )
+}
+
+export default DestinationDetail;
